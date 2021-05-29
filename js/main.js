d293adaa2df0f7d7009c7ff54892babada004157
Hopperys <vano_ust.kz@mail.ru>
2021-05-29 15:10:52 +0600

    Внес правки

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-const randomizeNumber = function (min, max) {
+const getRandomNumber = function (min, max) {
   if (min < 0 || max < 0) {
     return 'Числа должны быть положительными';
   }
+
   if (min >= max) {
     return 'Второе число должно быть больше первого';
   }
+
   return Math.floor(Math.random() * (max - min + 1)) + min; //  Взято с сайта developer.mozilla.org
 };
 
-randomizeNumber(1, 100);
+getRandomNumber(1, 100);
 
 const stringLengthCheck = function (string, maxLength) {
   return (string.length <= maxLength);
