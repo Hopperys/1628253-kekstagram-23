6174e9b1ae8efee1f7f45e54cb82f9d196518b7d
Hopperys <vano_ust.kz@mail.ru>
2021-06-05 02:11:04 +0600

    Добавил большинство функций

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+const MIN_ID_NUMBER = 1;
+const MAX_ID_NUMBER = 25;
+const MIN_LIKE_NUMBER = 15;
+const MAX_LIKE_NUMBER = 200;
+const MIN_AVATAR_NUMBER = 1;
+const MAX_AVATAR_NUMBER = 6;
+
+const PHOTO_COUNT = 25;
+
+const DESCRIPTIONS = [
+  'Фото моего завтрака',
+  'Красивый пейзаж',
+  'Отдых на море',
+  'Мой кот',
+  'Вид из самолета',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'Ваня',
+  'Саша',
+  'Катя',
+  'Настя',
+  'Витя',
+  'Маша',
+  'Петя',
+];
+
 const getRandomNumber = function (min, max) {
   if (min < 0 || max < 0) {
     return 'Числа должны быть положительными';
@@ -10,10 +46,39 @@ const getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //  Взято с сайта developer.mozilla.org
 };
 
-getRandomNumber(1, 100);
-
 const stringLengthCheck = function (string, maxLength) {
   return (string.length <= maxLength);
 };
 
 stringLengthCheck('Hello world', 20);
+
+const getRandomArrayElement = function (elements) {
+  return elements[getRandomNumber(0, elements.length -1)];
+};
+
+const generateRandomComment = function () {
+  const commentObject = {};
+
+  commentObject.id = getRandomNumber(1, 150);
+  commentObject.avatar = `img/avatar-${getRandomNumber(MIN_AVATAR_NUMBER, MAX_AVATAR_NUMBER)}.svg`;
+  commentObject.message = getRandomArrayElement(MESSAGES);
+  commentObject.name = getRandomArrayElement(NAMES);
+
+  return commentObject;
+};
+
+const createPhotoObject = function () {
+  const photoObject = {};
+
+  photoObject.id = getRandomNumber(MIN_ID_NUMBER, MAX_ID_NUMBER);
+  photoObject.url = `photos/${getRandomNumber(MIN_ID_NUMBER, MAX_ID_NUMBER)}.jpg`;
+  photoObject.description = getRandomArrayElement(DESCRIPTIONS);
+  photoObject.likes = getRandomNumber(MIN_LIKE_NUMBER, MAX_LIKE_NUMBER);
+  photoObject.comments = generateRandomComment();
+
+  return  photoObject;
+};
+
+const generatePhotosArray = new Array(PHOTO_COUNT).fill(null).map(() => createPhotoObject());
+
+generatePhotosArray;
