b93559551a5f98b4c2ad315efc08402df8c640b7
Hopperys <vano_ust.kz@mail.ru>
2021-06-09 09:34:02 +0600

    Добавил модули

---
+++ js/util.js
@@ -0,0 +1,23 @@
+const getRandomNumber = function (min, max) {
+  if (min < 0 || max < 0) {
+    return 'Числа должны быть положительными';
+  }
+
+  if (min >= max) {
+    return 'Второе число должно быть больше первого';
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min; //  Взято с сайта developer.mozilla.org
+};
+
+const stringLengthCheck = function (string, maxLength) {
+  return (string.length <= maxLength);
+};
+
+stringLengthCheck('Hello world', 20);
+
+const getRandomArrayElement = function (elements) {
+  return elements[getRandomNumber(0, elements.length -1)];
+};
+
+export {getRandomNumber, getRandomArrayElement};
