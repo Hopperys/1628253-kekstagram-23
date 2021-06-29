bc22cb5c90cd38071b511af87ae477488fbe4fcf
Hopperys <vano_ust.kz@mail.ru>
2021-06-29 19:13:48 +0600

    Добавил эффекты и скейл изображения

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
-import { isEscEvent} from './util.js';
+import {setImageScale} from './picture-scale.js';
+import {isEscEvent} from './util.js';
 
 const uploadFile = document.querySelector('#upload-file');
 const uploadOverlay = document.querySelector('.img-upload__overlay');
@@ -6,10 +7,13 @@ const uploadCancelButton = uploadOverlay.querySelector('#upload-cancel');
 const effectValue = uploadOverlay.querySelector('.effect-level__value');
 const hashTagsInput = uploadOverlay.querySelector('.text__hashtags');
 const pictureDescription = uploadOverlay.querySelector('.text__description');
+const imagePreview = document.querySelector('.img-upload__preview');
+const sliderWrapper = document.querySelector('.img-upload__effect-level');
 
 const validity =  /^#[a-zA-Zа-яА-я0-9]{1,19}$/;
 
 const MAX_TAGS_COUNT = 5;
+const DEFAULT_SCALE_VALUE = 100;
 
 const closeModal = () => {
   uploadOverlay.classList.add('hidden');
@@ -18,11 +22,14 @@ const closeModal = () => {
   effectValue.value = '';
   hashTagsInput.value = '';
   pictureDescription.value = '';
+  setImageScale(DEFAULT_SCALE_VALUE);
+  imagePreview.style.filter = 'none';
 };
 
 const openModal = () => {
   uploadOverlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
+  sliderWrapper.classList.add('visually-hidden');
 };
 
 const modalKeydownHandler = (evt) => {
